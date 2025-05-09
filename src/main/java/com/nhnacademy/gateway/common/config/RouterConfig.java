41963f98ab80ef7283e19e7098775165ca204a2f
Jihyun3478 <leejihyun3478@gmail.com>
2025-05-09 10:32:36 +0900

    fix: eureka 설정 수정

--- src/main/java/com/nhnacademy/gateway/common/config/RouterConfig.java
+++ src/main/java/com/nhnacademy/gateway/common/config/RouterConfig.java
@@ -9,31 +9,33 @@ import org.springframework.context.annotation.Configuration;
 public class RouterConfig {
     @Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        // Eureka 등록 기반 자동 분산
         return builder.routes()
-            .route("Ink3-API",
-                p -> p.path("/api/hello")
+            .route("shop-service",
+                p -> p.path("/api")
                     .filters(f -> f.stripPrefix(1))
-                    .uri("lb://INK3-API")
+                    .uri("lb://SHOP-SERVICE")
+            )
+            .route("front-service",
+                p -> p.path("/front")
+                    .filters(f -> f.stripPrefix(1))
+                    .uri("lb://FRONT-SERVICE")
+            )
+            .route("auth-service",
+                p -> p.path("/auth")
+                    .filters(f -> f.stripPrefix(1))
+                    .uri("lb://AUTH-SERVICE")
             )
             .build();
 
-        //http://localhost:80/hello
-
-        //        return builder.routes()
-        //                .route("hello-service-a",
-        //                        p->p.path("/hello").and().weight("hello",50).uri("http://localhost:8081/")
-        //                        )
-        //                .route("hello-service-b",
-        //                        p->p.path("/hello").and().weight("hello",50).uri("http://localhost:8082/")
-        //                        )
-        //                .build();
-
-        //        return builder.routes()
-        //                .route("get_route", r -> r.path("/account")
-        //                        .filters(o->o.addRequestHeader("uuid", UUID.randomUUID().toString()))
-        //                        .uri("http://httpbin.org"))
-        //                .build();
-
-        //http://httpbin.org/get
+        // 고정 서버 주소에 수동 분산
+        // return builder.routes()
+        //     .route("hello-service-a",
+        //         p -> p.path("/hello").and().weight("hello", 50).uri("http://localhost:8081/")
+        //     )
+        //     .route("hello-service-b",
+        //         p -> p.path("/hello").and().weight("hello", 50).uri("http://localhost:8082/")
+        //     )
+        //     .build();
     }
 }
